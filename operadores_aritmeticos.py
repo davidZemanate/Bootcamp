35d671368b0192cbeb4e8bbba421360a8b36f8fc
David Fernando Zemanate Zemanate <davidfzemanate@gmail.com>
2024-07-22 18:49:24 -0500

    operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,19 @@
+#suma y resta
+suma=2024+2000
+print("la suma es:",suma)
+resta=23-3
+print("la resta es:",resta)
+multiplicacion=3*3
+print("la multipliacion es:",multiplicacion)
+potencia=2**3
+print("la potencia es:",potencia)
+division_entera=12//4
+print("la división es:",division_entera)
+division=12//4
+print("la división es:",division)
+#resto
+resto=12%4
+print(resto)
+print(suma,resta,multiplicacion)
+tipo_de_dato=type(division_entera)
+print(tipo_de_dato)
